c991409bde9325dd58532d1723bb3a91e77162b6
Christopher Mercadante <cmercada86@gmail.com>
2016-08-17 22:13:23 +0000

    Adding serial communication protocols

---
+++ SerialBus/serialcomm.go
@@ -0,0 +1,7 @@
+package communications
+
+import "github.com/tarm/serial"
+
+type SerialComm struct {
+	config *serial.Config
+}
--- gardenbotcomm.go
+++ gardenbotcomm.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/go-mangos/mangos/protocol/rep"
+	//"github.com/go-mangos/mangos/protocol/req"
+	"github.com/go-mangos/mangos/transport/tcp"
+)
+
 func main() {
+
+	sock, err := rep.NewSocket()
+
+	if err != nil {
+		log.Fatalf("Can't get new response socker: %s\n", err)
+		os.Exit(1)
+	}
+
+	sock.AddTransport(tcp.NewTransport())
+
+	if err = sock.Listen(":88"); err != nil {
+		log.Fatalf("Can't get new response socker: %s\n", err)
+	}
 
 }
